86d9a38a8eeff00e13ca61c02ef8ca6fb605ad34
Pavel Kyurkchiev <pkyurkchiev@sbnd.net>
2019-05-07 17:10:31 +0300

    modified client administration

--- src/WebApplications/WebSPA.Administration/ClientApp/src/app/app.component.ts
+++ src/WebApplications/WebSPA.Administration/ClientApp/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { SecurityService } from './shared/services/security.service';
+import { ConfigurationService } from './shared/services/configuration.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientApp';
+
+  Authenticated: boolean = false;
+  subscription: Subscription;
+
+  constructor(private securityService: SecurityService, 
+    private configurationService: ConfigurationService) {
+    this.Authenticated = this.securityService.IsAuthorized;
+  }
+
+  ngOnInit() {
+    console.log('app on init');
+    this.subscription = this.securityService.authenticationChallenge$.subscribe(res => this.Authenticated = res);
+
+    //Get configuration from server environment variables:
+    console.log('configuration');
+    this.configurationService.load();
+  }
 }
